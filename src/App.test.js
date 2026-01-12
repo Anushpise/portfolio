be7b6ec83aedaff1e856443f59537213ba3fbebc
Anushpise <anshupise95@gmail.com>
2026-01-12 17:31:37 +0530

    remove error CI pipeline

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders loader image', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const imgElement = screen.getByAltText(/loading/i);
+  expect(imgElement).toBeInTheDocument();
 });
